7e8104e97bb075439da2e7d56a48c6fe7ef89c30
Pearyman <liyananhappy@sina.cn>
2015-08-04 12:23:27 +0800

    add common.css

---
+++ python/demo01.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+# first comment
+#print "hello world";
+# 
+#raw_input("\n\n Press the enter key");
+#a,b,c=1,2,"jhon";
+#print b;
+#a=1;
+#b=1;
+#c=0;
+#c=a+b;
+#print c;
+count=0;
+while(count<9):
+	print "the count is",count;
+	count=count+1;
+
+print "good bye";
